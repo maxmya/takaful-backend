0c834f05ba266c6bc80eb2addf1dfc1ff750244c
Marwa <marwa.gamal@intercom.com.eg>
2020-03-08 23:46:54 +0200

    search medications using medication name

--- src/main/kotlin/com/takaful/backend/service/freamwork/MedicationsService.kt
+++ src/main/kotlin/com/takaful/backend/service/freamwork/MedicationsService.kt
@@ -5,6 +5,6 @@ import com.takaful.backend.data.to.MedicationsDTO
 import com.takaful.backend.utils.Pageable
 
 interface MedicationsService {
-    fun getAllMedications(page: Int, size: Int): Pageable<*>
+    fun getAllMedications(page: Int, size: Int,query:String): Pageable<*>
     fun convertMedicationEntityToDTO(medicine: Medication): MedicationsDTO
 }
